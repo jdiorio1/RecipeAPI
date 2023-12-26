b091ba6dec1cf9f6b24d0ee22109f552484d326f
jdiorio1 <j.diorio@yahoo.com>
2023-12-26 12:34:19 -0500

    update directions to data type dictionary.

--- RecipeAPI/Models/Recipe.cs
+++ RecipeAPI/Models/Recipe.cs
@@ -3,7 +3,7 @@
     public class Recipe
     {
         public int Id { get; set; }
-        public List<string>? Directions { get; set; }
+        public Dictionary<int,string>? Directions { get; set; }
         public List<string>? Ingredients { get; set; }
         public DateOnly DateAdded { get; set; }
         public string? Source { get; set; }
