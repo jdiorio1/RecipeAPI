7c8dac3f41c90758daeca1513b564cc9f2040719
jdiorio1 <j.diorio@yahoo.com>
2024-01-04 11:31:27 -0500

    added Recipe Controller and post and get methods.

---
+++ RecipeAPI/Controllers/RecipesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly RecipeContext _context;
+
+        public RecipesController(RecipeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Recipes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            if (_context.Recipes == null)
+            {
+                return NotFound();
+            }
+            var recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        // POST: api/Recipes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.RecipeId }, recipe);
+        }      
+    }
+}
